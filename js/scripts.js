f7686a0f8adb4c4b38ce44170838b79d9b837238
arachchi <nuran.11@cse.mrt.ac.lk>
2016-01-16 14:49:29 +0530

    remove google analytics

--- js/scripts.js
+++ js/scripts.js
@@ -345,5 +345,5 @@ $(window).load(function() {
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-52115242-5', 'mediumra.re');
+ga('create', '', '');
 ga('send', 'pageview');
